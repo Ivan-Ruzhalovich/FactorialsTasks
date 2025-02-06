c1ec0d17957c2a8d49a79175b96efd8ffc6c9017
Ivan-Ruzhalovich <ruzhalovich@bk.ru>
2025-02-06 13:40:51 +0300

    Add files via upload

--- src/main/java/com/Ivan/FactorialTask.java
+++ src/main/java/com/Ivan/FactorialTask.java
@@ -11,15 +11,12 @@ public class FactorialTask  extends RecursiveTask<Long> {
 
     @Override
     protected Long compute() {
-        boolean negative = false;
         if(n<0) {
-            negative = true;
-            n *= -1;
+            throw new ArithmeticException("The factorial of a negative number is indeterminate");
         }
-        if (n==1 || n==0) return negative ? (long)-1 : (long)1;
+        if (n==1 || n==0) return (long)1;
         FactorialTask factorialTaskRecurs = new FactorialTask(n-1);
         factorialTaskRecurs.fork();
-        long result = factorialTaskRecurs.join()*n;
-        return !negative ? result : result * (-1);
+        return factorialTaskRecurs.join()*n;
     }
 }
